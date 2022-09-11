fb895d2b3faad412919cbcf349d366699086c5f2
Anouar <anobel1725@gmail.com>
2022-09-11 12:14:52 +0100

    More Styles and DarkMode

--- script.js
+++ script.js
@@ -1,28 +1,59 @@
-const darkbtn = document.querySelector('.darkmode');
+const darkbtn = document.getElementById('darkmode');
 const main = document.querySelector('.main');
 const aside = document.querySelector('.aside');
 const header = document.querySelector('.header');
 const article = document.querySelector('.article');
-const box = document.querySelectorAll('.box')
+const box = document.querySelectorAll('.box');
+const antr = document.querySelectorAll('.antr');
+
+function colorize(){
+    main.style.backgroundColor = "black";
+    
+    aside.style.backgroundColor = "rgb(80,80,80)";
+    aside.style.color = "black";
+
+    header.style.backgroundColor = "rgb(80,80,80)";
+    header.style.color = "black";
+
+    article.style.backgroundColor = "black";
+    article.style.color = "white"
+    // Changing the photo of the button.
+    document.getElementById('darkbtn_img').src = "./icons/white-balance-sunny.svg";
+    // Adding the black-border class to all the boxes. 
+    for(i of box){
+        i.classList.add('black-border');
+    }
+    for(i of antr){
+        i.classList.add('antr-blacked')
+    }
+}
+
 
 darkbtn.addEventListener('click', ()=>{
 
-    main.classList.toggle('.main')
-    main.style.backgroundColor = "black";
+    darkbtn.classList.toggle('blacked')
+    console.log("you have clicked and the it contains the blacked class"+darkbtn.classList.contains('blacked'))
+    if(darkbtn.classList.contains('blacked')){    
+        colorize();
+    }else if(!(darkbtn.classList.contains('blacked'))){
+        main.style.backgroundColor = "white";
 
-    aside.classList.toggle('.aside')
-    aside.style.backgroundColor = "grey";
-    aside.style.color = "black";
-
-    header.classList.toggle('.header');
-    header.style.backgroundColor = "grey";
-    header.style.color = "black";
-
-    article.classList.toggle('.article')
-    article.style.backgroundColor = "black";
-    article.style.color = "white"
-
-    box.style.borderLeft = "1px solid white"
-
+        aside.style.backgroundColor = "rgba(0, 0, 0, .4)";
+        aside.style.color = "white";
+    
+        header.style.backgroundColor = "rgba(0, 0, 0, .4)";
+        header.style.color = "white";
+    
+        article.style.backgroundColor = "white";
+        article.style.color = "black";
+        document.getElementById('darkbtn_img').src = "./icons/weather-night.svg"
+        // removing the balck-border class from all the boxes
+        for(i of box){
+            i.classList.remove('black-border')
+        }
+        for(i of antr){
+            i.classList.remove('antr-blacked')
+        }
+    }
 
 })
